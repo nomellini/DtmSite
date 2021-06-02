0ebbb2ef7e9344b0d5789a85277225f749613a38
gabriel.dassie <gabriel.dassie@hotmail.com>
2021-06-02 17:01:23 +0000

    Updated webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: 'http://api.datamace.com.br'
+            apiUrl: 'https://api.datamace.com.br'
         })
     }
 }
