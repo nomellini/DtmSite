c70fa7399ff97cf2240306bc2ba73a24c4b447ae
Gabriel Dassie <gabriel.dassie@telefonicabigdata.onmicrosoft.com>
2021-06-05 16:45:25 -0300

    ajuste api

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
     externals: {
         // global app config object
         config: JSON.stringify({
-            apiUrl: 'https://localhost:44368'
+            apiUrl: 'https://api.datamace.com.br'
         })
     }
 }
